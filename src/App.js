d82141a89c79c7185395f6e93413fe31fce31480
locdacknownothing <98312612+locdacknownothing@users.noreply.github.com>
2023-09-13 23:40:52 +0700

    update libraries

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from '~/components/Layout';
 import { Fragment } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
     return (
