2296b96ab221f41899011ed75897e469dbd0dc9f
locdacknownothing <98312612+locdacknownothing@users.noreply.github.com>
2023-09-25 09:44:20 +0700

    update footer contacts

--- src/components/Layout/DefaultLayout/Footer/index.js
+++ src/components/Layout/DefaultLayout/Footer/index.js
@@ -3,26 +3,34 @@ import './footer.scss';
 
 function Footer() {
     return (
-        <div className="container d-flex footer-container">
-            <span className="copy-right">Copyright © 2023 HNL-Goal Coding</span>
-            <nav>
-                <div className="container-fluid">
-                    <div className="d-flex">
-                        <Link class="footer-nav" to="/">
-                            Home
-                        </Link>
-                        <Link class="footer-nav" to="/about">
-                            About Us
-                        </Link>
-                        <Link class="footer-nav" to="/support">
-                            Support
-                        </Link>
-                        <Link class="footer-nav" to="/privacy">
-                            Privacy Policy
-                        </Link>
-                    </div>
-                </div>
-            </nav>
+        <div className="container footer-container">
+            <div className="footer-navbar">
+                <div className="footer-secondary me-3">Copyright © 2023 HNL-Goal Coding</div>
+                <Link class="footer-nav" to="/">
+                    Home
+                </Link>
+                <Link class="footer-nav" to="/about">
+                    About Us
+                </Link>
+                <Link class="footer-nav" to="/support">
+                    Support
+                </Link>
+                <Link class="footer-nav" to="/privacy">
+                    Privacy Policy
+                </Link>
+            </div>
+            <div className="footer-contacts">
+                <span className="footer-secondary">Contact us</span>
+                <Link to="https://www.facebook.com/">
+                    <img src="/images/facebook.svg" alt="facebook" className="footer-contact" />
+                </Link>
+                <Link to="https://www.linkedin.com/">
+                    <img src="/images/linkedin.svg" alt="linkedin" className="footer-contact" />
+                </Link>
+                <Link to="https://www.instagram.com/">
+                    <img src="/images/instagram.svg" alt="instagram" className="footer-contact" />
+                </Link>
+            </div>
         </div>
     );
 }
